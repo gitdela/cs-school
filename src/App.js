fe4bc55ba6e4b0225786ed267995c637ebd3634a
gitdela <scriptingdela@gmail.com>
2023-06-11 09:44:16 +0000

    Edit routes

--- src/App.js
+++ src/App.js
@@ -1,19 +1,19 @@
 import React from 'react';
-import { HashRouter, Route, Routes } from 'react-router-dom';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import Home from './Pages/Home';
 import Calendar from './Pages/Calendar';
 
 function App() {
   return (
     <div>
-      <HashRouter>
+      <BrowserRouter>
         <Routes>
           {/* <Route index element={<Home />} /> */}
           <Route path='/home' element={<Home />} />
           <Route path='/' element={<Home />} />
           <Route path='/Calendar' element={<Calendar />} />
         </Routes>
-      </HashRouter>
+      </BrowserRouter>
     </div>
   );
 }
--- src/Components/UpcomingEvents.jsx
+++ src/Components/UpcomingEvents.jsx
@@ -1,52 +1,57 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
 
 const UpcomingEvents = () => {
   return (
     <div className='max-w-screen-xl px-4 mx-auto'>
-      <h1 class='font-bold text-center text-2xl py-8 mb-3'>
+      <h1 className='font-bold text-center text-2xl py-8 mb-3'>
         Upcoming Events at Crystal Heights School
       </h1>
 
       <div className='w-full space-y-4 sm:gap-4 text-sm sm:w-1/2'>
-        <div class='card-container grid grid-cols-3 gap-2 w-full bg-white shadow-md'>
+        <div className='card-container grid grid-cols-3 gap-2 w-full bg-white shadow-md'>
           <div className='bg-customOrange col-span-1/2 flex justify-center'>
-            <div class='date  flex flex-col justify-center items-center text-gray-700'>
-              <div class='day font-light'>02</div>
-              <div class='month font-semibold'>JUNE</div>
+            <div className='date  flex flex-col justify-center items-center text-gray-700'>
+              <div className='day font-light'>02</div>
+              <div className='month font-semibold'>JUNE</div>
             </div>
           </div>
-          <div class='info-container col-span-2 py-2 flex flex-col justify-center items-start'>
-            <div class='event-name font-bold pb-2 text-base'>Revision Week</div>
-            <div class='event-location font-light'>
+          <div className='info-container col-span-2 py-2 flex flex-col justify-center items-start'>
+            <div className='event-name font-bold pb-2 text-base'>
+              Revision Week
+            </div>
+            <div className='event-location font-light'>
               Students revise their notes this week for exams
             </div>
           </div>
         </div>
-        <div class='card-container grid grid-cols-3 gap-2 w-full bg-white shadow-md'>
+        <div className='card-container grid grid-cols-3 gap-2 w-full bg-white shadow-md'>
           <div className='bg-customOrange col-span-1/2 flex justify-center'>
-            <div class='date  flex flex-col justify-center items-center text-gray-700'>
-              <div class='day font-light'>09</div>
-              <div class='month font-semibold'>JUNE</div>
+            <div className='date  flex flex-col justify-center items-center text-gray-700'>
+              <div className='day font-light'>09</div>
+              <div className='month font-semibold'>JUNE</div>
             </div>
           </div>
-          <div class='info-container col-span-2 py-2 flex flex-col justify-center items-start'>
-            <div class='event-name font-bold pb-2 text-base'>Exams Start</div>
-            <div class='event-location font-light'>
+          <div className='info-container col-span-2 py-2 flex flex-col justify-center items-start'>
+            <div className='event-name font-bold pb-2 text-base'>
+              Exams Start
+            </div>
+            <div className='event-location font-light'>
               Summer Exams before the long holidays
             </div>
           </div>
         </div>
-        <div class='card-container grid grid-cols-3 gap-2 w-full bg-white shadow-md'>
+        <div className='card-container grid grid-cols-3 gap-2 w-full bg-white shadow-md'>
           <div className='bg-customOrange col-span-1/2 flex justify-center'>
-            <div class='date  flex flex-col justify-center items-center text-gray-700'>
-              <div class='day font-light'>24</div>
-              <div class='month font-semibold'>JUNE</div>
+            <div className='date  flex flex-col justify-center items-center text-gray-700'>
+              <div className='day font-light'>24</div>
+              <div className='month font-semibold'>JUNE</div>
             </div>
           </div>
-          <div class='info-container col-span-2 py-2 flex flex-col justify-center items-start'>
-            <div class='event-name font-bold pb-2 text-base'>Vacation Day</div>
-            <div class='event-location font-light'>
+          <div className='info-container col-span-2 py-2 flex flex-col justify-center items-start'>
+            <div className='event-name font-bold pb-2 text-base'>
+              Vacation Day
+            </div>
+            <div className='event-location font-light'>
               Students vacate today for the long holidays
             </div>
           </div>
